e3500e670ad1b5c91aaabcf87dc55b6e87600ca9
Bryandu <Bryandu>
2021-03-09 20:39:56 -0300

    Fiz errors of typescript

--- src/components/anchor/anchor.tsx
+++ src/components/anchor/anchor.tsx
@@ -2,13 +2,12 @@ import Link from 'next/link';
 import React, { AnchorHTMLAttributes } from 'react';
 
 import { AnchorText, BtnAnchor } from './styles';
-
 interface Anchor extends AnchorHTMLAttributes<HTMLAnchorElement> {
   button?: boolean;
 }
 const Anchor = ({ button, ...props }: Anchor) => {
   return (
-    <Link href={props.href} passHref>
+    <Link href={`${props.href}`} passHref>
       {button ? (
         <BtnAnchor {...props}>{props.children}</BtnAnchor>
       ) : (
