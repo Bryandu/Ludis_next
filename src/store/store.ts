4a97e3236a6836cee0d8fd0218c4fc5bf9cf2e52
bryandu <bryan.corcetti@gmail.com>
2021-04-27 20:15:12 -0300

    Push branch

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,7 +2,6 @@ import { createWrapper, MakeStore } from 'next-redux-wrapper';
 import { applyMiddleware, combineReducers, createStore, Store } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import createSagaMiddleware, { Task } from 'redux-saga';
-import { Context } from 'vm';
 
 import reducer from './ducks/user/userReducer';
 import { UserState } from './ducks/user/userTypes';
@@ -16,8 +15,7 @@ const reducers = combineReducers({
   user: reducer
 });
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const makeStore: MakeStore<UserState> = (context: Context) => {
+const makeStore: MakeStore<UserState> = () => {
   const sagaMiddleware = createSagaMiddleware();
   const store: SagaStore = createStore(
     reducers,
